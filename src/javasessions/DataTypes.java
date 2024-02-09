4f23ade412733bb1f51030813c67491b5861ab9d
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-02-09 14:00:05 +0900

    first commit

---
+++ src/javasessions/DataTypes.java
@@ -0,0 +1,34 @@
+package javasessions;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Java supports strict data types:
+		//A. Primitive data types: It will just take some space in the memory 
+		//No need to create the object of the class
+			//Numeric :
+				//Integral: 
+					//a. Integer: byte, short, int, long 
+					//b. Floating-point: float, double 
+					//c. Char: char
+			//Boolean: true/false 
+		
+		//B. Non-primitive data types:String, Class, Interface, Arrays, Abstract Class
+		
+		//1. byte:
+		byte b = 10;
+		byte b1 = 20;
+		b1 = 30;
+		b1 = 40;
+		System.out.println(b1);
+		//1 byte = 8 bits
+		
+		//long:
+		//8 bytes = 8*8 = 64 bits
+		long l = 10;
+		
+	}
+
+}
