c8ddac6c29cfd8ea56783a2213aef7bd5bcfc752
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-02-22 11:02:03 +0900

    added linkedLnQuiz

---
+++ src/linkedlnQuestions/Test22022024.java
@@ -0,0 +1,15 @@
+package linkedlnQuestions;
+
+public class Test22022024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int ar[]=new int[-5];
+		for(int i=0;i<ar.length;i++) {
+			System.out.println(ar[i]);
+		}
+
+	}
+
+}
