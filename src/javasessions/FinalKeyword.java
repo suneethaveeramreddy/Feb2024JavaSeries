bb30de29c2f460dedd75c3e0e6eea1740824eb7e
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-02-23 07:34:07 +0900

    added FinalKeyword

---
+++ src/javasessions/FinalKeyword.java
@@ -0,0 +1,24 @@
+package javasessions;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//final -- constant values
+		
+		int a=10;
+		a=20;
+		
+		final int days=7;
+		//days=10;
+		int totalAmount=100*days;
+		System.out.println(totalAmount);
+		
+		final String title="Login Page";
+		final double basePrice =12.22;
+		
+
+	}
+
+}
