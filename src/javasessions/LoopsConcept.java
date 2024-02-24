5d6909e54de6bd11e498445e9c840306d4e3305b
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-02-24 11:07:28 +0900

    added LoopsConcept

---
+++ src/javasessions/LoopsConcept.java
@@ -0,0 +1,29 @@
+package javasessions;
+
+public class LoopsConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1 to 10:
+		//1. while loop:
+		
+		int i =1;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		int l=1;
+		while(l<=100)
+		{
+			if(l==5) {
+				System.out.println("hi");
+			}
+			System.out.println(l);
+			l++;
+		}
+	}
+
+}
