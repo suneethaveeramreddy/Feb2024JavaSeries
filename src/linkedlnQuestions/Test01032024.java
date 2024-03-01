5729a2d306fe78c1bcd6a66d3a3d7fdf90fd4356
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-01 15:10:39 +0900

    added LinkedlnQuiz

---
+++ src/linkedlnQuestions/Test01032024.java
@@ -0,0 +1,17 @@
+package linkedlnQuestions;
+
+public class Test01032024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		System.out.println(x>2?x<4?10:8:7);// This is same as ==> x>2?(x<4?10:8):7
+		/*
+		 * conditional operators
+		 * variable x = (expression) ? value if true : value if false
+		 */
+				
+
+	}
+
+}
---
+++ src/linkedlnQuestions/Test01March2024.java
@@ -0,0 +1,18 @@
+package linkedlnQuestions;
+
+public class Test01March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=1280;
+		Integer num2=num1;
+		Integer num3=num2;
+		System.out.println(num1==num2);
+		System.out.println(num2==num3);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+
+	}
+
+}
