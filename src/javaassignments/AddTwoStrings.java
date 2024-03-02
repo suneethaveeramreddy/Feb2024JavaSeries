6facf37559bce9db5685ead503b720795cf42007
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-03 06:23:19 +0900

    added Assignments package

---
+++ src/javaassignments/AddTwoStrings.java
@@ -0,0 +1,13 @@
+package javaassignments;
+
+public class AddTwoStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a ="Hello";
+		String b ="Naveen K";
+		System.out.println(a+" "+b);
+
+	}
+
+}
---
+++ src/javaassignments/DivisionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package javaassignments;
+
+public class DivisionOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=50;
+		int b=3;
+		int c=a/b;
+		System.out.println(c);
+		
+	}
+
+}
