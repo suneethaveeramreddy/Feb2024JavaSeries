dc391e291e52871319e23a973139d55b1eba1578
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-06 12:41:42 +0900

    edited ArraysExamples

--- src/javasessions/ArraysExamples.java
+++ src/javasessions/ArraysExamples.java
@@ -30,9 +30,50 @@ public class ArraysExamples {
 		System.out.println(Arrays.toString(player1));
 		System.out.println(Arrays.toString(player));
 		
+		//String Array:
+		String st[]=new String[5];
+		st[0] = "Amazon";
+		st[1] = "Raza";
+		System.out.println(Arrays.toString(st));
 		
+		//int 
+		int i[]=new int[5];
+		i[0] = 100;
+		System.out.println(Arrays.toString(i));
 		
+		short sh[]=new short[5];
+		sh[0] = 200;
+		System.out.println(Arrays.toString(sh));
 		
+		//double:
+		double db[]=new double[5];
+		db[0] = 200.12;
+		System.out.println(Arrays.toString(db));
+		
+		//char:
+		char ch[] = new char[5];
+		ch[0] = 'a';
+		System.out.println(Arrays.toString(ch));
+		
+		//boolean:
+		boolean b[]=new boolean[5];
+		b[0]=true;
+		System.out.println(Arrays.toString(b));
+		
+		//int, short, long, byte:0
+		//double, float:0.0
+		//char: blank space
+		//boolean: false
+		//non-primitive: String, Object: always NULL
+		
+		//
+		int num[] = {2,4,55,1,20,67,900};
+		System.out.println(Arrays.toString(num));
+		num[0]=100;
+		System.out.println(Arrays.toString(num));
+		
+		//num[9]=500;
+		//System.out.println(Arrays.toString(num));
 	}
 
 }
