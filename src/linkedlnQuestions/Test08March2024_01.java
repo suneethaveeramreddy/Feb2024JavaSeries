6089850fe9415a4a7417c1d2944b9189227245b2
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-09 06:16:08 +0900

    Added LinkedLnQuiz

---
+++ src/linkedlnQuestions/Test08March2024_01.java
@@ -0,0 +1,13 @@
+package linkedlnQuestions;
+
+public class Test08March2024_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i='a';
+		System.out.println(i);
+
+	}
+
+}
---
+++ src/linkedlnQuestions/Test09March2024.java
@@ -0,0 +1,14 @@
+package linkedlnQuestions;
+
+public class Test09March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte x=127;
+		x++;
+		x++;
+		System.out.println(x);
+
+	}
+
+}
