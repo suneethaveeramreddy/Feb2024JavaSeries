80b54ad6158a3ba12330c94bbb88dc40dd345fff
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-11 13:18:58 +0900

    edited ArrayListConcept

--- src/javasessions/ArrayListConcept.java
+++ src/javasessions/ArrayListConcept.java
@@ -1,6 +1,7 @@
 package javasessions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListConcept {
 
@@ -12,13 +13,15 @@ public class ArrayListConcept {
 		
 		//Create the object of ArrayList class using new keyword
 		//index based/order based array:
+		//when we initialize an array the VC=10 & PC=0
 		ArrayList ar=new ArrayList();
+		System.out.println(ar.size());//pc=0
 		
 		ar.add(100);//0
 		ar.add(200);//1
 		ar.add(300);//2
 		
-		System.out.println(ar.size());
+		System.out.println(ar.size());//pc=3 & vc=7
 		
 		ar.add(400);
 		ar.add(500);
@@ -33,7 +36,31 @@ public class ArrayListConcept {
 		System.out.println(ar.size());
 		System.out.println(ar.get(0));
 		System.out.println(ar.get(8));
-		System.out.println(ar.get(9));//IOB 
+		//System.out.println(ar.get(9));//IOB 
+		
+		ar.add(1100);
+		
+		//restrict the arraylist: generics in AL
+		
+		//integer data : AL :
+		
+		ArrayList<Integer> marksList= new ArrayList<Integer>();//vc=10 , pc=0
+		marksList.add(100);
+		marksList.add(1200);
+		
+		ArrayList<Double> bmiList=new ArrayList<Double>();
+		bmiList.add(12.33);
+		bmiList.add(100.0);
+		
+		//int --> Integer
+		//double --> Double 
+		
+		//diff types of data in AL:
+		ArrayList<Object> empDataList=new ArrayList<Object>();
+		
+		
+		
+		
 
 	}
 
---
+++ src/linkedlnQuestions/Test11March2024.java
@@ -0,0 +1,13 @@
+package linkedlnQuestions;
+
+public class Test11March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="Hellow";
+		System.out.println(str.indexOf('t'));
+
+	}
+
+}
