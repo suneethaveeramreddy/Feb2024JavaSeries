539ddbd44ffd931249fa0ff0562b3efdefcf265d
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-15 11:10:04 +0900

    Added LinkedlnQuiz

--- src/ifelsestatements/WAPToCheckNumIsOddOrEven.java
+++ src/ifelsestatements/WAPToCheckNumIsOddOrEven.java
@@ -1,14 +1,34 @@
 package ifelsestatements;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WAPToCheckNumIsOddOrEven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<String> ar=new ArrayList<String>(20);
+		ArrayList<String> ar=new ArrayList<String>(5);
+		//vc=5, pc=0
+		//5/2 = 2
 		
+		ArrayList<Integer> marksList=new ArrayList<Integer>();
+		marksList.add(1);//0
+		marksList.add(2);//1
+		marksList.add(3);//2
+		marksList.add(4);//3
+		
+		marksList.remove((Object)2);
+		System.out.println(marksList);
+		
+		//ArrayList Literals:
+		ArrayList<String> browserList=new ArrayList<String>(Arrays.asList("chrome","firefox","edge"));
+		System.out.println(browserList);
+		System.out.println(browserList.size());
+		System.out.println(browserList.get(0));
+		for(String e:browserList) {
+			System.out.println(e);
+		}
 	}
 
 }
---
+++ src/linkedlnQuestions/Test15March2024.java
@@ -0,0 +1,13 @@
+package linkedlnQuestions;
+
+public class Test15March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1="one";
+		String str2="two";
+			System.out.println(str1.concat(str2));
+
+	}
+
+}
