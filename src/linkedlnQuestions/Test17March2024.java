3db9ffe885d55359b9ccaeb5fcbd2307f901762e
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-17 06:00:56 +0900

    Added Linkedln Quiz

---
+++ src/linkedlnQuestions/Test17March2024.java
@@ -0,0 +1,14 @@
+package linkedlnQuestions;
+
+public class Test17March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i;
+		for(i=1;i<6;i++) {
+			if(i>3)
+				continue;
+		}
+		System.out.println(i);
+	}
+}
