0fd509b8869876411b0862daacdf3a01aff2b845
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-22 05:54:40 +0900

    Added linkedln Quiz

---
+++ src/linkedlnQuestions/Test22March2024.java
@@ -0,0 +1,13 @@
+package linkedlnQuestions;
+
+public class Test22March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char chars[]= {'a','b','c'};
+		String s=new String(chars);
+		System.out.println(chars);
+
+	}
+
+}
