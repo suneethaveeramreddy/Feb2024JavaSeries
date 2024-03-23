6dcc38d3391cfa946e89aab809bfadc459d53df3
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-23 14:43:24 +0900

    Added ClassAndObjects

---
+++ src/classandobjects/Employee.java
@@ -0,0 +1,13 @@
+package classandobjects;
+
+public class Employee {
+	
+		//Class --> Category, BluePrint 
+		//Object: physical entity created from Class(Category)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i=10; //local variable
+			
+	}
+
+}
