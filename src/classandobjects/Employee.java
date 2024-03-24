f4ee37762e1847e8f37ca9a731601f304d572e58
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-25 06:25:47 +0900

    Added Linkedln Quiz

--- src/classandobjects/Employee.java
+++ src/classandobjects/Employee.java
@@ -2,8 +2,14 @@ package classandobjects;
 
 public class Employee {
 	
-		//Class --> Category, BluePrint 
-		//Object: physical entity created from Class(Category)
+		//Class --> Category, BluePrint, Templates 
+		//Object: physical entity created from Class(Specific Category)
+		//Every object will have its own properties
+	
+		String name;
+		int age;
+		double salary;
+		String city;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			int i=10; //local variable
---
+++ src/linkedlnQuestions/Test25March2024.java
@@ -0,0 +1,15 @@
+package linkedlnQuestions;
+
+public class Test25March2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int ar[]= {1,2,3,4,5};
+		for(int i=0;i<ar.length-2;++i) {
+			System.out.print(ar[i]+ " ");
+		}
+
+	}
+
+}
