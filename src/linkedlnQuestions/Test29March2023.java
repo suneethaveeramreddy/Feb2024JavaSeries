ccc90187e8fd8f8572e1cd01e74cc23495e474a8
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-03-29 05:40:21 +0900

    Added Linkedln Quiz

---
+++ src/linkedlnQuestions/Test29March2023.java
@@ -0,0 +1,14 @@
+package linkedlnQuestions;
+
+public class Test29March2023 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String c="Hello,How are you?";
+		boolean var;
+		var=c.startsWith("hello");
+		System.out.println(var);
+
+	}
+
+}
