e0f861bbe59f026df8b05577221b453ef1d70343
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-01 09:30:13 +0900

    added linkedlnQuiz

---
+++ src/linkedlnQuizApril2024/Test01April2024.java
@@ -0,0 +1,14 @@
+package linkedlnQuizApril2024;
+
+public class Test01April2024 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	
+	public static void main(int[] args) {
+			System.out.println("Hello");
+
+	}
+
+}
