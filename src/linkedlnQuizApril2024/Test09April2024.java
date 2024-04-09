d10f165f9401059a888f3307f55839ef40e2b5ec
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-09 09:23:03 +0900

    Added Linkedln Quiz

---
+++ src/linkedlnQuizApril2024/Test09April2024.java
@@ -0,0 +1,17 @@
+package linkedlnQuizApril2024;
+
+public class Test09April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(true) {
+			//break;
+			/*
+			 * Ans : Compilation Error, Because Break statement can only be used with loop
+			 * or switch. You can't use break to exit an if statement unless the if
+			 * statement is inside of a loop.
+			 */		
+			}
+	}
+
+}
