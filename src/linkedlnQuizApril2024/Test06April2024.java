960ed84f3d399e4ef2d91118054660fbe5670a89
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-10 09:47:05 +0900

    Added Linkedln Quiz

---
+++ src/linkedlnQuizApril2024/Test06April2024.java
@@ -0,0 +1,18 @@
+package linkedlnQuizApril2024;
+
+public class Test06April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char chars[]= {'a','b','c'};
+		String s=new String(chars);
+		System.out.println(s);
+		/*
+		 * Explanation: When we construct a String from a character array using the new
+		 * String(chars) which is String constructor, it constructs a String by
+		 * concatenating all the characters in the array in the order they appear so
+		 * internally it would be a+b+c (concatenation) so result would be : abc
+		 */
+	}
+
+}
---
+++ src/linkedlnQuizApril2024/Test08April2024.java
@@ -0,0 +1,16 @@
+package linkedlnQuizApril2024;
+
+public class Test08April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num[]= {5.5,10.1,11,12.8,56.9,2.5};
+		double result;
+		result=0;
+		for(int i=0;i<6;i++) 
+			result=result+num[i];
+			System.out.println(result/6);
+
+	}
+
+}
