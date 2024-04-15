665b217f70d88012194cd09792b2df0533558deb
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-16 08:58:00 +0900

    Added Linkedln Quiz

---
+++ src/linkedlnQuizApril2024/Test16April2024.java
@@ -0,0 +1,18 @@
+package linkedlnQuizApril2024;
+
+public class Test16April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=20;
+		switch (number) {
+		case 10:
+			System.out.println("10");
+		case 20:
+			System.out.println("20");
+		case 30:
+			System.out.println("30");
+		}
+	}
+
+}
