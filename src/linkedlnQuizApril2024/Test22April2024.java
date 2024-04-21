d2acf0cc6d88cb650a39785c9316cda5e2a7fd1c
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-22 08:54:03 +0900

    Added LinkedlnQuiz

---
+++ src/linkedlnQuizApril2024/Test22April2024.java
@@ -0,0 +1,15 @@
+package linkedlnQuizApril2024;
+
+public class Test22April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Sachin";
+		String s2="Sachin";
+		String s3=new String("Sachin");
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+
+	}
+
+}
