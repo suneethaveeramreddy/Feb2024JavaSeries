f39ebc8de3c3220227c5feaf8b151cc18815d18f
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-22 09:27:12 +0900

    Added LinkedlnQuiz

---
+++ src/linkedlnQuizApril2024/Test21April2024.java
@@ -0,0 +1,15 @@
+package linkedlnQuizApril2024;
+
+public class Test21April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c='1';
+		System.out.println(c);//1
+		System.out.println((int)c);//1
+		System.out.println(String.valueOf(c));//1
+		int a = Integer.parseInt(String.valueOf(c));//1
+		System.out.println(a);//1
+	}
+
+}
