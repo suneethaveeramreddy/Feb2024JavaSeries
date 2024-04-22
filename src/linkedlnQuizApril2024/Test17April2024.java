1710e151354ca6cdebb5748e3e1e7887f02ad2d2
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-22 13:36:23 +0900

    Added LinkedlnQuiz

---
+++ src/linkedlnQuizApril2024/Test17April2024.java
@@ -0,0 +1,22 @@
+package linkedlnQuizApril2024;
+
+public class Test17April2024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x=3.14;
+		System.out.println(x);//3.14
+		double k = Math.ceil(x);
+		System.out.println(k);//4.0
+		int y=(int) Math.ceil(x);
+		System.out.println(y);//4
+		
+		/*
+		 * Java Math ceil()
+		 * The ceil() method rounds the specified double value upward and returns it.
+		 * The rounded value will be equal to the mathematical integer. That is, the
+		 * value 3.24 will be rounded to 4.0 which is equal to integer 4.
+		 */
+	}
+
+}
