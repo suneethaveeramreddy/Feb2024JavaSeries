cc883e09baade80bd4e6d67fd7e7f1ec6903db98
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-25 20:49:15 +0900

    added linkedlnQuiz

---
+++ src/linkedlnQuizSaravanan/QuizDay105.java
@@ -0,0 +1,17 @@
+package linkedlnQuizSaravanan;
+
+public class QuizDay105 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] numbers= {1,2,3,4,5};
+		for(int num: numbers) {
+			num*=2;
+		}
+		for(int num: numbers) {
+			System.out.print(num+" ");
+		}
+
+	}
+
+}
