e30e4d8c5754891449792507d031da9ca153ab01
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-04-26 12:27:33 +0900

    added linkedln Quiz

---
+++ src/linkedlnQuizSaravanan/QuizDay96.java
@@ -0,0 +1,17 @@
+package linkedlnQuizSaravanan;
+
+public class QuizDay96 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=3;
+		System.out.println(a/b);//1
+		System.out.println(a%b);//2
+		int res=(a/b*b+a%b);//5
+		System.out.println(res);
+		System.out.println(res+a++-++b);//5+5-4===6
+
+	}
+
+}
--- src/linkedlnQuizSaravanan/QuizDay97.java
+++ src/linkedlnQuizSaravanan/QuizDay97.java
@@ -4,7 +4,13 @@ public class QuizDay97 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(fibonacci(5));
 	}
-
+	
+	public static int fibonacci(int n) {
+		if(n<=1) {
+			return n;
+		}
+		return fibonacci(n-1)+fibonacci(n-2);//f(4)+f(3)== f(1)+0+f(1)+f(1)+0+f(1)+0+f(1)=5
+	}
 }
