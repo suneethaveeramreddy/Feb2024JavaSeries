65eb8f77874fc043a1b10cb922c4872c0e0ea216
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-05-01 12:17:04 +0900

    added new package geeksForGeeks

---
+++ src/geeksForGeeks/ReadAndPrintAnIntegerValue.java
@@ -0,0 +1,17 @@
+package geeksForGeeks;
+
+import java.util.Scanner;
+
+public class ReadAndPrintAnIntegerValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int varname = 0;
+		System.out.println("Please enter the number=");
+		Scanner se=new Scanner(System.in);
+		varname=se.nextInt();
+		System.out.println("The Entered number:" +varname);
+
+	}
+
+}
