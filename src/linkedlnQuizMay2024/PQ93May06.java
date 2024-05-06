dd4fc92006c9886053a44732a1b0ceb8973ff138
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-05-07 08:40:08 +0900

    added package linkedlnQuizMay2024

---
+++ src/linkedlnQuizMay2024/PQ93May06.java
@@ -0,0 +1,11 @@
+package linkedlnQuizMay2024;
+
+public class PQ93May06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=50+30+"sachin"+40+40;
+		System.out.println(s);
+	}
+
+}
---
+++ src/linkedlnQuizMay2024/PQ94May07.java
@@ -0,0 +1,15 @@
+package linkedlnQuizMay2024;
+
+public class PQ94May07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			if(i==5) {
+				break;
+			}
+			System.out.print(i+ " ");
+		}
+	}
+
+}
