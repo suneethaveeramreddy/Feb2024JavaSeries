7c4a56e70a316efb36a0b6add284531fb2afabf0
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-05-11 14:31:53 +0900

    added new package

---
+++ src/javaDevelopersCommunity/PQMay11.java
@@ -0,0 +1,14 @@
+package javaDevelopersCommunity;
+
+public class PQMay11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		System.out.println(x/0);
+		boolean result=(x==0)&&(x/0>10);
+		System.out.println(result);
+
+	}
+
+}
