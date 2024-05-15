43cf518309a5ac46b2cd23fa1ccb36c2efe82c2b
Suneetha Veeramreddy <suneetha.pec@gmail.com>
2024-05-15 13:12:21 +0900

    added linkedlnQuiz

---
+++ src/linkedlnQuizMay2024/PQ98May15.java
@@ -0,0 +1,18 @@
+package linkedlnQuizMay2024;
+
+public class PQ98May15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Apple");
+	}
+	public static void main(char args[]) {
+		System.out.println("Orange");
+	}
+	
+	public static void main(int args[]) {
+		System.out.println(10);
+	}
+	
+
+}
